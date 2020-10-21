7c6b34eaf33c4235bc653ea463ac49dbc66b48b4
valdecirinovec <valdecirinovec@gmail.com>
2020-10-21 16:32:01 -0300

    php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document PHP </title>
+</head>
+<body>
+            <H1>TESTANDO PHP </H1>
+            <?php
+            echo "Olá mundo!"
+            ?>
+</body>
+</html>
